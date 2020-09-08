242a9b91972a507699e6899e49bc45a4acc1b284
Wisam Al-Ali <44989955+weesooo@users.noreply.github.com>
2020-09-07 22:10:19 -0500

    deploy portfolio to Github

---
+++ precache-manifest.85408ef39fd1a4b561157b91c1a6688e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f62b9835cfc4c75986d846d9be487dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2a0da42deb5500f826a",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a16f740f6aaa6807b9b1",
+    "url": "/static/js/2.bb0ee620.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.bb0ee620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a0da42deb5500f826a",
+    "url": "/static/js/main.2a26c66b.chunk.js"
+  },
+  {
+    "revision": "3b174c0570773bff4976",
+    "url": "/static/js/runtime-main.7867e5dd.js"
+  }
+]);
